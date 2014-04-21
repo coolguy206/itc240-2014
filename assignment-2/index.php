03b69cd62e5b4a6d96aebfdbc521e5667e8a802f
David Thanphilom <davidthanphilom@msn.com>
2014-04-21 02:30:55 -0700

    added another number input field

--- assignment-2/index.php
+++ assignment-2/index.php
@@ -4,6 +4,12 @@
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <title>Assignment 2</title>
+<style>
+b {
+	color: #f00;
+}
+</style>
+
 </head>
 
 <body>
@@ -21,7 +27,10 @@
 <input type="text" name="adjective" placeholder="adjective">
 
 <label for="number">a Number between 1-10</label>
-<input type="text" name="number" placeholder="number">
+<input type="text" name="number1" placeholder="number">
+
+<label for="number">a Number between 1-10</label>
+<input type="text" name="number2" placeholder="number">
 
 <button>STORY TIME</button>
 
@@ -34,52 +43,47 @@ if ($_SERVER['REQUEST_METHOD']== "POST"){
 $noun = $_REQUEST['noun'];
 $verb = $_REQUEST['verb'];
 $adjective = $_REQUEST['adjective'];
-$number = $_REQUEST['number'];
+$number1 = $_REQUEST['number1'];
+$number2 = $_REQUEST['number2'];
 
 
 ?>
 
 
 <p>
-It's just another rainy night in Seattle, the hero of our story <b><?= htmlentities($noun); ?></b> is busy making sure our streets are safe. As <b><?= htmlentities($noun); ?></b> was <b><?= htmlentities($verb); ?></b> there was a loud womanly 
+It's just another rainy night in Seattle, the lead of our story a <b><?= htmlentities($adjective); ?></b> costumed hero by the name of <b><?= htmlentities($noun); ?></b> is busy making sure our streets are safe. As <b><?= htmlentities($noun); ?></b> was <b><?= htmlentities($verb); ?></b> there was a loud womanly 
 shriek! <b><?= htmlentities($noun); ?></b> jumped to action and cartwheeled to the rescue.
 </p>
 
 <p>
-When <b><?= htmlentities($noun); ?></b> arrived the first thing the massively muscled hero noticed was a desperate woman screaming at the top of her lungs as
+When <b><?= htmlentities($noun); ?></b> arrived the first thing the massively muscled hero noticed was a desperate woman screaming at the top of her lungs as what looked liked mutated evildoers from the depths of hell were attacking this woman.
+</p>
 
-<?php if($number == 5) { ?>
+<?php if($number1 == 5) { ?>
+<p>
+ <b><?= htmlentities($noun); ?></b> considered the option of calling for backup due to the grotesque features of the villains.
+</p>
 
- <b><?= htmlentities($number); ?></b> mutated villains that looked like the spawn of hell were attacking this woman. <b><?= htmlentities($noun); ?></b> sprang to action and rushed the <b><?= htmlentities($number); ?></b> 
-villains and with one spinning heel kick, wiped out the <b><?= htmlentities($number); ?></b> attackers. 
+<?php } ?>
+ 
+
+<?php if($number2 > 5) { ?>
+<p>
+Our hero <b><?= htmlentities($noun); ?></b>, realizing that being greatly out numbered one to <b><?= htmlentities($number2); ?></b> decided that a frontal assault would not be the best plan. Instead <b><?= htmlentities($noun); ?></b> decided to take out the attackers 2 at a time quietly. Attacking the evildoer's pressure points causing them to explode to ashes!
 </p>
  
+
+<?php } else {  ?>
 <p>
-The woman was so relieved that she asked our hero <b><?= htmlentities($noun); ?></b>, if there was anything she could do to repay our hero for saving her life. Our hero said "As long as your safe." and with that our hero went <b><?= htmlentities($verb); ?></b> off into the darkness becoming legends of stories. 
-
-
-<?php } else if ($number > 5) { ?>
-
-<b><?= htmlentities($number); ?></b> mutated villains that looked like the spawn of hell were attacking this woman. Our hero <b><?= htmlentities($noun); ?></b>, realizing that being greatly out numbered decided that a frontal assault would not be the best plan. Instead <b><?= htmlentities($noun); ?></b> decided to take out the attackers 2 at a time quietly. Attacking the 
-villain's pressure points causing them to crumple and making them harmless.
+<b><?= htmlentities($noun); ?></b> laughed at the evildoers and with one flex of our hero's muscle bound body caused the <b><?= htmlentities($number2); ?></b> attackers to run off screaming in fear of our hero.
 </p>
 
-<p>
-The woman was so relieved that she asked our hero <b><?= htmlentities($noun); ?></b>, if there was anything she could do to repay our hero for saving her life. Our hero said "As long as your safe." and with that our hero went <b><?= htmlentities($verb); ?></b> off into the darkness becoming legends of stories. 
-
-<?php } else { ?>
-
-<b><?= htmlentities($number); ?></b> mutated villains that looked like the spawn of hell were attacking this woman. <b><?= htmlentities($noun); ?></b> laughed at the <b><?= htmlentities($number); ?></b> 
-villains and with one flex of our hero's muscle bound body caused the <b><?= htmlentities($number); ?></b> 
-villains to run off cowardly screaming in fear of our hero.
-</p>
+<?php } ?>
 
 <p>
 The woman was so relieved that she asked our hero <b><?= htmlentities($noun); ?></b>, if there was anything she could do to repay our hero for saving her life. Our hero said "As long as your safe." and with that our hero went <b><?= htmlentities($verb); ?></b> off into the darkness becoming legends of stories. 
 
-<?php }
-
-} ?>
+<?php } ?>
 
 </p>
 </body>
