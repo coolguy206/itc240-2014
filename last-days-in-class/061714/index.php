35278ed7325183c09980862d101dfe16bed33245
coolguy206 <davidthanphilom@msn.com>
2014-06-17 19:38:04 -0700

    last days in class

---
+++ last-days-in-class/061714/index.php
@@ -0,0 +1,11 @@
+<?php
+//get contents
+$contents = file_get_contents("sample.txt");
+
+echo $contents;
+
+/*$add = "Some more text to add to the sample.txt file!";
+$file = file_put_contents("sample.txt", $add);
+
+echo $file;*/
+
