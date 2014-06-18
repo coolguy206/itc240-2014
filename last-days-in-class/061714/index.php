83226be87848aebeab4a3256d8c4cbc969034805
coolguy206 <davidthanphilom@msn.com>
2014-06-17 20:18:12 -0700

    modified code

--- last-days-in-class/061714/index.php
+++ last-days-in-class/061714/index.php
@@ -2,10 +2,34 @@
 //get contents
 $contents = file_get_contents("sample.txt");
 
-echo $contents;
-
 /*$add = "Some more text to add to the sample.txt file!";
 $file = file_put_contents("sample.txt", $add);
 
 echo $file;*/
 
+//(na)+ group like AND single character
+//[na]+ any of the letters contained
+//"." any character
+//"*" previous character 
+//"+" previous character one time
+// d\ digit
+// D\ not digit
+// \s space
+// \S not space
+// \w letters and _ (underscore)
+// \W not letters, numbers, and _ (underscore)
+
+//for more info go to http://www.regular-expressions.info/
+
+$replace =  preg_replace("#ba[na]+#i", //find all s characters
+"S", //replace with S 
+$contents // inside this string
+);
+
+$replace = preg_replace(
+"#page [0-9,]+\s*\n#",
+"",
+$replace
+);
+
+echo nl2br($replace);
